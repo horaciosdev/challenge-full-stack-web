28dafedb545b8f65016239409ba9d03a7a893686
Horacio Schumann Oliveira <horacio.schumann@gmail.com>
2025-03-15 02:23:03 -0300

    feat: simplifica as rotas utilizando resource

--- grupoa/start/routes.ts
+++ grupoa/start/routes.ts
@@ -7,5 +7,9 @@
 |
 */
 
+import StudentsController from '#controllers/students_controller';
+
 import router from '@adonisjs/core/services/router'
 router.on('/').renderInertia('home')
+
+router.resource('students', StudentsController)
