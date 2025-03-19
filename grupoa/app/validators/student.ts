78ccb697803418fd6a4b72ef85f463daa1e7878b
Horacio Schumann Oliveira <horacio.schumann@gmail.com>
2025-03-18 21:06:51 -0300

    refactor: remove parâmetro não utilizado

--- grupoa/app/validators/student.ts
+++ grupoa/app/validators/student.ts
@@ -26,21 +26,21 @@ export const updateStudentValidator = vine.withMetaData<{ studentId: number }>()
       table: 'students',
       column: 'email',
       caseInsensitive: true,
-      filter: (db, value, field) => {
+      filter: (db, _, field) => {
         db.whereNot('id', field.meta.studentId);
       }
     }),
     ra: vine.string().trim().unique({
       table: 'students',
       column: 'ra',
-      filter: (db, value, field) => {
+      filter: (db, _, field) => {
         db.whereNot('id', field.meta.studentId);
       }
     }).optional(),
     cpf: vine.string().trim().regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/).unique({
       table: 'students',
       column: 'cpf',
-      filter: (db, value, field) => {
+      filter: (db, _, field) => {
         db.whereNot('id', field.meta.studentId);
       }
     }).optional()
