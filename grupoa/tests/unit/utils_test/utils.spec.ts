eb6966b880b0df4ee5a3e99ffb2799c652e7c50e
Horacio Schumann Oliveira <horacio.schumann@gmail.com>
2025-03-19 00:46:30 -0300

    test: atualiza a descrição dos testes

--- grupoa/tests/unit/utils_test/utils.spec.ts
+++ grupoa/tests/unit/utils_test/utils.spec.ts
@@ -2,8 +2,8 @@ import { test } from '@japa/runner'
 import { cpfMask } from '../../../utils/mask_utils.js';
 import { justNumbers } from '../../../utils/string_utils.js';
 
-test.group('Utils: cpfMask', () => {
-  test('formata CPF corretamente', ({ assert }) => {
+test.group('Utils', () => {
+  test('must format the cpf correctly', ({ assert }) => {
     const testCases = [
       { input: '12345678901', expected: '123.456.789-01' }, // Caso completo
       { input: '00000000000', expected: '000.000.000-00' }, // Zeros
@@ -15,7 +15,7 @@ test.group('Utils: cpfMask', () => {
     })
   })
 
-  test('retorna somente números de uma string', ({ assert }) => {
+  test('returns only the numbers from the input string', ({ assert }) => {
     const input = 'A1B2C3#4$5%6~7[8]9{0}1';
 
     assert.equal(justNumbers(input), '12345678901');
